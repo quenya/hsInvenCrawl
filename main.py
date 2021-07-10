f1f78c079d7fed6d2d9c3c64cee2dd59214994de
이종현 <gear.yi@miracom.co.kr>
2021-07-10 16:45:55 +0900

    add chrome driver crawling hs inven parse articles ˆ

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_chrome_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')  # 웹 브라우저를 띄우지 않는 headless chrome 옵션 적용
+    options.add_argument('disable-gpu')  # GPU 사용 안함
+    options.add_argument('lang=ko_KR')  # 언어 설정
+    return webdriver.Chrome('chromedriver', options=options)  # 옵션 적용
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hs Inven Crawler')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    driver = get_chrome_driver()
+    hs_inven_url = "https://www.inven.co.kr/board/hs/3508?come_idx=3508&category=유저"
+    driver.get(hs_inven_url)
+    doc_list = driver.find_elements_by_xpath("//tr[contains(@class, 'ls') and contains(@class, 'oh') and contains(@class, 'tr')]")
+    print(doc_list)
+    for doc in doc_list:
+        num = doc.find_element_by_class_name('bbsNo').text
+        article = doc.find_element_by_class_name('sj_ln')
+        title = article.text
+        href = article.get_property('href')
+        print('%s - %s: %s' % (num, title, href))
