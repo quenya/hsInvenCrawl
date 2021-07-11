e07dad1e0d3c235aa9e554c906ccf64baaad6af4
이종현 <gear.yi@miracom.co.kr>
2021-07-11 15:09:52 +0900

    email util try finally

--- email_util.py
+++ email_util.py
@@ -4,17 +4,20 @@ import datetime
 
 
 def send_email():
-    smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp.starttls()  # TLS 사용시 필요
-    id = 'quendya7@gamil.com'
-    pw = 'wcqqvwqdfncxzzik'
-    smtp.login(id, pw)
-    msg = MIMEText('Hs inven crawler started')
-    msg['Subject'] = '[%s] Hs inven crawler started' % datetime.datetime.now()
-    email = 'qendya@naver.com'
-    msg['To'] = email
-    smtp.sendmail(id, email, msg.as_string())
-    smtp.quit()
+    try:
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.starttls()  # TLS 사용시 필요
+        id = 'quendya7@gamil.com'
+        pw = 'wcqqvwqdfncxzzik'
+        smtp.login(id, pw)
+        msg = MIMEText('Hs inven crawler started')
+        msg['Subject'] = '[%s] Hs inven crawler started' % datetime.datetime.now()
+        email = 'qendya@naver.com'
+        msg['To'] = email
+        smtp.sendmail(id, email, msg.as_string())
+        smtp.quit()
+    finally:
+        print('send mail was failed')
 
 
 send_email()
